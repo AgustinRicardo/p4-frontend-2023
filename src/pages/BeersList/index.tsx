13bef0a47fff020b9c921138c1403da72c10a216
Agustín Rodríguez <agustin.rodriguez@digittude.com>
2023-06-23 14:52:52 +0200

    Adding beers table component

--- src/pages/BeersList/index.tsx
+++ src/pages/BeersList/index.tsx
@@ -1,9 +1,32 @@
-import { Heading, VStack } from "@chakra-ui/react";
+import { Heading, VStack, Text } from "@chakra-ui/react";
+import React from "react";
+import { getBeers } from "../../services/beers";
+import BeersTable from "../../component/BeersTable";
 
 const BeersList = () => {
+  const [beers, setBeers] = React.useState([]);
+
+  const getBeersFromAPI = async () => {
+    const currentBeers = await getBeers();
+    setBeers(currentBeers);
+  };
+
+  React.useEffect(() => {
+    getBeersFromAPI();
+  }, []);
+
   return (
     <VStack spacing={8}>
       <Heading>Beers List</Heading>
+ 
+      {beers.length ? (
+        <>
+          <Text>({beers.length} beers today)</Text>
+          <BeersTable beers={beers} />
+        </>
+      ) : (
+        <Text>(No beers today)</Text>
+      )}
     </VStack>
   );
 };
