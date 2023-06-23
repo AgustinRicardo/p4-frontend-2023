8fbe092a63f5ecb6f11030eb2aec9f503f5d4dd2
Agustín Rodríguez <agustin.rodriguez@digittude.com>
2023-06-23 16:04:11 +0200

    Adding initial router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import BeersList from "./pages/BeersList";
+import { Route, Routes } from "react-router";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
-        <BeersList />
+        <Routes>
+          <Route path="/" element={<BeersList />} />
+        </Routes>
       </Grid>
     </Box>
   </ChakraProvider>
