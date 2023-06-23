bbbea9785ef8b8a31d2bdc8ccd77c4b031a82899
Agustín Rodríguez <agustin.rodriguez@digittude.com>
2023-06-23 12:57:33 +0200

    Adding BeersList component

---
+++ src/pages/BeersList/index.tsx
@@ -0,0 +1,11 @@
+import { Heading, VStack } from "@chakra-ui/react";
+
+const BeersList = () => {
+  return (
+    <VStack spacing={8}>
+      <Heading>Beers List</Heading>
+    </VStack>
+  );
+};
+
+export default BeersList;
